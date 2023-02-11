a68723c82d00b80667ec82e7b27f235eadc67f05
ch55secake <oscardj112@gmail.com>
2023-02-11 22:19:22 +0000

    basic card appears onscreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import BalanceCard from "./containers/home/balance-card";
 import DateCard from "./containers/home/date-card";
+import CategoryCard from "./containers/home/category-card";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
             <BalanceCard />
             <DateCard />
         </div>
+            <CategoryCard />
         </div>
       </header>
     </div>
--- frontend/src/containers/home/category-card/index.js
+++ frontend/src/containers/home/category-card/index.js
@@ -3,11 +3,11 @@ import { memo } from "react";
 import styles from "./index.module.css"
 
 const CategoryCard = () => {
-
     return (
-      <CardComponent width="27.5" maxHeight="25vh">
+      <CardComponent width="27.5%" maxHeight="30vh">
           <p className={styles.title}>Spending Categories</p>
       </CardComponent>
     );
-
 }
+
+export default memo(CategoryCard);
